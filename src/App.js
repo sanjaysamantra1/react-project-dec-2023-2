0e7baf6e3d8e0daf09ad562ea347a31a582ee5ac
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-01-23 21:30:11 +0530

    carousel

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import Header from './components/header/Header'
 import Main from './components/main/Main'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar';
+import Categories from './components/categories/Categories';
+import Carousel from './components/carousel/Carousel';
 
 export default function App() {
   return <div>
-    <Header />
+    {/* <Header /> */}
     <Navbar />
+    <Categories/>
+    <Carousel />
     <Main />
     <Footer />
   </div>
